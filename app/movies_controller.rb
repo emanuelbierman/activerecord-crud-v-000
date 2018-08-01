d37c5692396cf5144aa37ca44a3105b2b4accd47
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-01 14:24:06 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,7 +83,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.update_all(["release_date >= ?", 2000])
+  Movie.where(["release_date > ?", 1999]).update_all(title: "A Movie")
 end
 
 def can_destroy_a_single_item
